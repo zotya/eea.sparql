fb5b38bfa632e8c8b9de2cde3568413ab8414317
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-30 14:01:25 +0000

    Refs #4801; Cleaned up wrapper

--- eea/sparql/browser/sparql.py
+++ eea/sparql/browser/sparql.py
@@ -1,32 +1,14 @@
 from Products.Five import BrowserView
 from Products.ZSPARQLMethod.Method import ZSPARQLMethod, parse_arg_spec, map_arg_values, interpolate_query_html
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from time import time
 
 class SPARQL(BrowserView):
 
-    def __init__(self, *args, **kwargs):
-        BrowserView.__init__(self, *args, **kwargs)
-        self.method = ZSPARQLMethod(self.context.id, self.context.title, self.context.endpoint)
-
-    def execute(self, **arg_values):
-        self.method = ZSPARQLMethod(self.context.id, self.context.title, self.context.endpoint)
-        self.method.query = self.context.query()
-        if self.context.timeout > 0:
-            self.method.timeout = self.context.timeout
-        self.method.arg_spec = self.context.arguments
-
-        return self.method(**arg_values)
-
-    _test_html = ViewPageTemplateFile('test_query.pt', globals())
-    def test_query(self, REQUEST):
-        """
-        Execute the query and pretty-print the results as an HTML table.
-        """
-
-        self.method.arg_spec = self.context.arguments
-        arg_spec = parse_arg_spec(self.method.arg_spec)
-        missing, arg_values = map_arg_values(arg_spec, REQUEST.form)
+    def test_query(self):
+        arg_spec = parse_arg_spec(self.context.arg_spec)
+        missing, arg_values = map_arg_values(arg_spec, self.request.form)
         error = None
 
         if missing:
@@ -38,7 +20,7 @@ class SPARQL(BrowserView):
             t0 = time()
 
             try:
-                data = self.execute(**arg_values)
+                data = self.context.execute(**arg_values)
 
             except Exception, e:
                 import traceback
@@ -48,12 +30,11 @@ class SPARQL(BrowserView):
             dt = time() - t0
 
         options = {
-            'query': interpolate_query_html(self.method.query, arg_values),
+            'query': interpolate_query_html(self.context.query, arg_values),
             'data': data,
             'duration': dt,
             'arg_spec': arg_spec,
             'error': error,
         }
-        return self._test_html(REQUEST, **options)
 
-
+        return options
--- eea/sparql/content/sparql.py
+++ eea/sparql/content/sparql.py
@@ -8,14 +8,17 @@ from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 from Products.Archetypes.atapi import StringField, StringWidget, IntegerField, IntegerWidget, TextField, TextAreaWidget
 
-# -*- Message Factory Imported Here -*-
+from Products.ZSPARQLMethod.Method import ZSPARQLMethod, parse_arg_spec, map_arg_values
+
+from AccessControl import ClassSecurityInfo
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
 from eea.sparql.interfaces import ISPARQL
 from eea.sparql.config import PROJECTNAME
 
 SPARQLSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
     StringField(
-        name='endpoint',
+        name='endpoint_url',
         widget=StringWidget(
             label="SPARQL endpoint URL",
         ),
@@ -29,14 +32,14 @@ SPARQLSchema = schemata.ATContentTypeSchema.copy() + atapi.Schema((
         required=0
     ),
     StringField(
-        name='arguments',
+        name='arg_spec',
         widget=StringWidget(
             label="Arguments",
         ),
         required=0
     ),
     TextField(
-        name='query',
+        name='sparql_query',
         default_content_type = 'text/plain',
         allowable_content_types = ('text/plain',),
 
@@ -55,7 +58,7 @@ SPARQLSchema['description'].storage = atapi.AnnotationStorage()
 schemata.finalizeATCTSchema(SPARQLSchema, moveDiscussion=False)
 
 
-class SPARQL(base.ATCTContent):
+class SPARQL(base.ATCTContent, ZSPARQLMethod):
     """SPARQL"""
     implements(ISPARQL)
 
@@ -65,5 +68,22 @@ class SPARQL(base.ATCTContent):
     title = atapi.ATFieldProperty('title')
     description = atapi.ATFieldProperty('description')
 
+    security = ClassSecurityInfo()
+
+    @property
+    def query(self):
+        return self.sparql_query()
+
+    security.declarePublic("index_html")
+    def index_html(self):
+        """index_html"""
+        return self()
+
+    security.declarePublic("execute_query")
+    def execute_query(self, args=None):
+        """execute query"""
+        arg_spec = parse_arg_spec(self.arg_spec)
+        arg_values = map_arg_values(arg_spec, args)[1]
+        return self.execute(**self.map_arguments(**arg_values))
 
 atapi.registerType(SPARQL, PROJECTNAME)
