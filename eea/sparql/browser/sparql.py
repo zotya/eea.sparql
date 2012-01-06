2463abff8e6ccebe8a07c91137fb34d566084e00
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-01-06 11:07:07 +0000

    Refs #4854; added page to configure caching for sparql

--- eea/sparql/browser/sparql.py
+++ eea/sparql/browser/sparql.py
@@ -1,15 +1,18 @@
 """ sparql
 """
+
 from Products.Five import BrowserView
-from Products.ZSPARQLMethod.Method import parse_arg_spec, map_arg_values, \
-                                        interpolate_query_html
+from Products.ZSPARQLMethod.Method import interpolate_query_html
+from Products.ZSPARQLMethod.Method import map_arg_values
+from Products.ZSPARQLMethod.Method import parse_arg_spec
+from eea.sparql.converter.sparql2json import sparql2json
 from time import time
 import json
 
-from eea.sparql.converter.sparql2json import sparql2json
 
 class Sparql(BrowserView):
     """Sparql view"""
+
     def test_query(self):
         """test query"""
         arg_spec = parse_arg_spec(self.context.arg_spec)
@@ -48,3 +51,34 @@ class Sparql(BrowserView):
         """json"""
         data = self.context.execute_query()
         return json.dumps(sparql2json(data))
+
+
+class Caching(BrowserView):
+
+    def cache_managers(self):
+        return self.context.ZCacheable_getManagerIds()
+
+    def enabled(self):
+        return self.context.ZCacheable_enabled()
+
+    def current_manager(self):
+        return self.context.ZCacheable_getManagerId()
+
+    def __call__(self):
+        if not "submit" in self.request.form:
+            return self.index()
+
+        manager_id = self.request.form.get("manager_id","")
+        old_manager_id = self.context.ZCacheable_getManagerId()
+
+        if manager_id != old_manager_id:
+            self.context.ZCacheable_setManagerId(manager_id)
+
+        enabled = self.request.form.get("enabled")
+
+        if self.context.ZCacheable_enabled() != enabled:
+            self.context.ZCacheable_setEnabled(bool(enabled))
+
+        return self.index()
+
+
