e78cffc8178cf398e597f9d91eabd7fc05e74f14
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-06-09 17:57:12 +0300

    Refs #25448; WIP upgrade step for stopped sparql queries

--- eea/sparql/upgrades/evolve4400.py
+++ eea/sparql/upgrades/evolve4400.py
@@ -20,28 +20,36 @@ def restart_sparqls(context):
     pr = getToolByName(context, 'portal_repository')
     brains = catalog.searchResults(portal_type = 'Sparql')
     currentDate = datetime.datetime.now()
-    possibleBroken = []
-    once = []
-    noProblemo = []
-    tocheck = []
+    needsRestart = []
+    import pdb; pdb.set_trace()
+
+    """ stop all sparql jobs """
+    for queue in queues.values():
+        jobs = [job for job in queue.__iter__()]
+        for job in queue.__iter__():
+            shouldRemove = False
+            for arg in job.args:
+                if hasattr(arg, 'func_name') and arg.func_name == 'async_updateLastWorkingResults':
+                    shouldRemove = True
+            if shouldRemove:
+                queue.remove(job)
     import pdb; pdb.set_trace()
     for brain in brains:
         obj = brain.getObject()
         if obj.getRefresh_rate() != 'Once':
-            try:
-                history = pr.getHistoryMetadata(obj)
-                retrieve = history.retrieve
-                lastChange = retrieve(history.getLength(countPurged = False) - 1,
-                                     countPurged=False)
-                lastModStamp = lastChange['metadata']['sys_metadata']['timestamp']
-                lastModDate = datetime.datetime.fromtimestamp(lastModStamp)
-                delta = currentDate - lastModDate
-                if delta.days > 14:
-                    possibleBroken.append(obj.absolute_url())
-                else:
-                    noProblemo.append(obj.absolute_url())
-            except:
-                tocheck.append(obj.absolute_url())
-        else:
-            once.append(obj.absolute_url())
+            needsRestart.append(obj)
+#            try:
+#                history = pr.getHistoryMetadata(obj)
+#                retrieve = history.retrieve
+#                lastChange = retrieve(history.getLength(countPurged = False) - 1,
+#                                     countPurged=False)
+#                lastModStamp = lastChange['metadata']['sys_metadata']['timestamp']
+#                lastModDate = datetime.datetime.fromtimestamp(lastModStamp)
+#                delta = currentDate - lastModDate
+#                if delta.days > 14:
+#                    possibleBroken.append(obj.absolute_url())
+#                else:
+#                    noProblemo.append(obj.absolute_url())
+#            except:
+#                .append(obj.absolute_url())
     import pdb; pdb.set_trace()
