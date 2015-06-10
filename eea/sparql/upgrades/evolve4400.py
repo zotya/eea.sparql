1963d43e4ffa0033cdd218d86682e0edbad6f048
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-06-10 16:20:30 +0300

    Refs #25448; Added upgrade step to restart the sparql sync jobs

--- eea/sparql/upgrades/evolve4400.py
+++ eea/sparql/upgrades/evolve4400.py
@@ -2,10 +2,11 @@
 """
 
 import logging
-import datetime
 from Products.CMFCore.utils import getToolByName
 from zope.component import getUtility
 from plone.app.async.interfaces import IAsyncService
+from eea.sparql.content.sparql import async_updateLastWorkingResults
+import DateTime
 
 logger = logging.getLogger("eea.sparql.upgrades")
 
@@ -14,42 +15,20 @@ def restart_sparqls(context):
     """
 
     async = getUtility(IAsyncService)
-    queues = async.getQueues()
-
     catalog = getToolByName(context, 'portal_catalog')
-    pr = getToolByName(context, 'portal_repository')
     brains = catalog.searchResults(portal_type = 'Sparql')
-    currentDate = datetime.datetime.now()
-    needsRestart = []
-    import pdb; pdb.set_trace()
 
-    """ stop all sparql jobs """
-    for queue in queues.values():
-        jobs = [job for job in queue.__iter__()]
-        for job in queue.__iter__():
-            shouldRemove = False
-            for arg in job.args:
-                if hasattr(arg, 'func_name') and arg.func_name == 'async_updateLastWorkingResults':
-                    shouldRemove = True
-            if shouldRemove:
-                queue.remove(job)
-    import pdb; pdb.set_trace()
+    restarted = 0
     for brain in brains:
         obj = brain.getObject()
         if obj.getRefresh_rate() != 'Once':
-            needsRestart.append(obj)
-#            try:
-#                history = pr.getHistoryMetadata(obj)
-#                retrieve = history.retrieve
-#                lastChange = retrieve(history.getLength(countPurged = False) - 1,
-#                                     countPurged=False)
-#                lastModStamp = lastChange['metadata']['sys_metadata']['timestamp']
-#                lastModDate = datetime.datetime.fromtimestamp(lastModStamp)
-#                delta = currentDate - lastModDate
-#                if delta.days > 14:
-#                    possibleBroken.append(obj.absolute_url())
-#                else:
-#                    noProblemo.append(obj.absolute_url())
-#            except:
-#                .append(obj.absolute_url())
-    import pdb; pdb.set_trace()
+            obj.scheduled_at = DateTime.DateTime()
+            async.queueJob(async_updateLastWorkingResults,
+                            obj,
+                            scheduled_at = obj.scheduled_at,
+                            bookmarks_folder_added = False)
+            restarted += 1
+
+    message = 'Restarted %s Sparqls ...' %restarted
+    logger.info(message)
+    return message
